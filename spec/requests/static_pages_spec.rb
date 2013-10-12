d1a269dcc113b901077843438f4ea6972c98a586
Georges K <yoann.celton+c9@gmail.com>
2013-10-12 08:01:03 -0400

    A bit of content

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,14 +2,23 @@ require 'spec_helper'
 
 describe "Static pages" do
   subject{ page }
+  let(:base_title) { "Cook Me A Squid!" }
   
   describe "Home page" do
     before { visit home_path }
-    it {should have_selector('h1', text:"welcome")}
+    it {should have_title("#{base_title} | Home")}
+    it {should have_selector('h1', text:"Welcome")}
+    
+    it {should have_link("Home", href: home_path)}
+    it {should have_link("Help", href: help_path)}
   end
   
   describe "Help page" do
     before { visit help_path }
+    it {should have_title("#{base_title} | Help")}
     it {should have_selector('h1', text:"Are you lost?")}
+    
+    it {should have_link("Home", href: home_path)}
+    it {should have_link("Help", href: help_path)}
   end
 end
