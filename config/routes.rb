0c0280ff7e869de80d55513bb2ef015a6c710d9d
Georges K <yoann.celton+c9@gmail.com>
2013-10-14 07:54:58 -0400

    Adding signup path

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Cookmeasquid::Application.routes.draw do
-  get "users/new"
 
   match '/home',    to:'static_pages#home' ,    via: 'get'
   match '/help',    to:'static_pages#help' ,    via: 'get'
+  match '/signup',  to:'users#new'         ,    via: 'get'
   root              to:'static_pages#home'
 end
