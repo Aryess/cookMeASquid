3c8e8dfe544e1c52851c85255ff80f41bfdd96cd
Georges K <yoann.celton+c9@gmail.com>
2013-10-14 07:28:11 -0400

    Starting with User

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Cookmeasquid::Application.routes.draw do
+  get "users/new"
+
   match '/home',    to:'static_pages#home' ,    via: 'get'
   match '/help',    to:'static_pages#help' ,    via: 'get'
   root              to:'static_pages#home'
