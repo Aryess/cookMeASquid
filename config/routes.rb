e937fbeb2f501c734b39ac228e8df48c4d12a083
Georges K <yoann.celton+c9@gmail.com>
2013-10-17 04:45:41 -0400

    Simple user profile

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Cookmeasquid::Application.routes.draw do
-
+  resources :users
+  
   match '/home',    to:'static_pages#home' ,    via: 'get'
   match '/help',    to:'static_pages#help' ,    via: 'get'
   match '/signup',  to:'users#new'         ,    via: 'get'
