f6b6e57ebd2c8939c09d5898c7162eb9d9df075c
Georges K <yoann.celton+c9@gmail.com>
2013-10-21 08:48:57 -0400

    Adding test for invalid user

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -4,11 +4,21 @@ describe "User pages" do
   subject {page}
   
   describe "profile page" do
-    let(:user) { FactoryGirl.create(:user) }
-    before { visit user_path(user) }
-
-    it { should have_content(user.login) }
-    it { should have_title(user.login) }
+    
+    describe "with valid user" do
+      let(:user) { FactoryGirl.create(:user) }
+      before { visit user_path(user) }
+  
+      it { should have_content(user.login) }
+      it { should have_title(user.login) }
+    end
+    
+    describe "with invalid user" do
+      before { visit user_path(-1) }
+  
+      it { should_not have_content(user.login) }
+      it { should have_title("Home") }
+    end
   end
   
   describe "Signup page" do
