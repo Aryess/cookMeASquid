dbc508faf95768697e79e3a665922ba53e3c75ba
Georges K <yoann.celton+c9@gmail.com>
2013-10-24 09:39:23 -0400

    I can signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,26 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
   
   def show
     begin
-      @user = User.find(params[:id])
+      @user ||= User.find(params[:id])
     rescue
+      flash[:error] = "Nothing to see here!"
       redirect_to root_path
+    end
+  end
+  
+  def create
+   
+    @user = User.new(params[:user])
+    if(@user.save)
+      flash[:success] = "Registration successful!"
+      redirect_to @user
+    else
+      flash[:error] = "Something went wrong..."
+      render 'new'
     end
   end
 end
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -21,9 +21,33 @@ describe "User pages" do
     end
   end
   
-  describe "Signup page" do
-    before {visit signup_path}
+  describe "signup" do
+    before { visit signup_path }
+    let(:submit) { "Create my account" }
+
     it{should have_content "Sign up"}
     it{should have_title "Sign up"}
+    
+    describe "with invalid information" do
+      it "should not create a user" do
+        expect { click_button submit }.not_to change(User, :count)
+      end
+    end
+
+    describe "with valid information" do
+      before do
+        fill_in "Login",        with: "Jdoe"
+        fill_in "Name",         with: "John"
+        fill_in "Surname",      with: "Mc Doe"
+        fill_in "Age",          with: 42
+        fill_in "Email",        with: "jdoe@example.com"
+        fill_in "Password",     with: "foobar"
+        fill_in "Confirmation", with: "foobar"
+      end
+
+      it "should create a user" do
+        expect { click_button submit }.to change(User, :count).by(1)
+      end
+    end
   end
 end
