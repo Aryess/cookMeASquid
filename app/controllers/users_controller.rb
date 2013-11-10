c56de0054ff5fd4b37751ab543a53722d9cd4f61
Georges K <yoann.celton+c9@gmail.com>
2013-11-10 11:10:38 -0500

    Sessions done right

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  before_filter :signed_in_user, only: [:edit, :update]
+  before_filter :correct_user,   only: [:edit, :update]
+  
   def new
     @user = User.new
   end
@@ -13,26 +16,41 @@ class UsersController < ApplicationController
   end
   
   def create
-   
     @user = User.new(params[:user])
     if(@user.save)
       flash[:success] = "Registration successful!"
       sign_in @user
-      redirect_to @user
+      redirect_back_or @user
     else
       render 'new'
     end
   end
   
+  def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(params[:user])
+      flash[:success] = "Update successful!"
+      sign_in @user
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+  
   private
-
-    def user_params
-      params.require(:user).permit( :name,
-                                    :surname,
-                                    :age,
-                                    :login,
-                                    :email, 
-                                    :password,
-                                    :password_confirmation)
+    def signed_in_user
+      unless signed_in?
+        store_location
+        redirect_to signin_url, notice: "Please sign in."
+      end
+    end
+    
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
     end
 end
